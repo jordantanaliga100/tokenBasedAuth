645bf5b2322eff0ad357fa8d2fe35281a56ac000
tanaliga100 <jordantanaliga@gmail.com>
2025-07-14 15:15:16 +0800

    chore(docs): added flowchart for visualization

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const connectDB = async (connectionString: any) => {
-  try {
-    if (!connectionString) {
-      throw new Error("MONGO_URL is undefined. Check your .env file.");
-    }
+// export const connectDB = async (connectionString: any) => {
+//   try {
+//     if (!connectionString) {
+//       throw new Error("MONGO_URL is undefined. Check your .env file.");
+//     }
 
-    mongoose.set("strictQuery", true);
-    await mongoose.connect(connectionString as string);
-  } catch (error) {
-    console.log("DB ERROR", error);
-  }
-};
+//     mongoose.set("strictQuery", true);
+//     await mongoose.connect(connectionString as string);
+//   } catch (error) {
+//     console.log("DB ERROR", error);
+//   }
+// };
